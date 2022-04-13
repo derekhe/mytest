66820df82b9a6389b921650aba6bd36ba8add58f
hesicong <derekhe@april1985.com>
2022-04-13 14:21:02 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
     led.plot(0, 0)
+    led.unplot(0, 0)
 })
 input.onButtonPressed(Button.B, function () {
     servos.P0.stop()
 })
 basic.forever(function () {
     kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 0)
+    radio.sendValue("name", 0)
 })
