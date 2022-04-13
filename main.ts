5f1fb0e10ffe6ab7d9acb9dc7eb724cc2d5f9e82
hesicong <derekhe@april1985.com>
2022-04-13 14:20:20 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
+    led.plot(0, 0)
 })
 input.onButtonPressed(Button.B, function () {
     servos.P0.stop()
